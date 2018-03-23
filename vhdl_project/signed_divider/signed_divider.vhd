7efd20c8c569eb4cad162f9e0d5527ba5b95296b
Frederic St-Pierre <frederic.st-pierre.7@ulaval.ca>
2018-03-23 17:02:14 -0400

    added basic divider, fixed signed multiplier test bench

---
+++ vhdl_project/signed_divider/signed_divider.vhd
@@ -0,0 +1,63 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:38:13 03/23/2018 
+-- Design Name: 
+-- Module Name:    signed_divider - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity signed_divider is
+ generic ( SIGNAL_LENGTH: positive);
+ PORT(
+		input_A : IN  std_logic_vector(SIGNAL_LENGTH-1 downto 0);
+		input_B : IN  std_logic_vector(SIGNAL_LENGTH-1 downto 0);
+		clk : IN  std_logic;
+		reset : IN  std_logic;
+		en : IN  std_logic;
+		output : OUT  std_logic_vector(SIGNAL_LENGTH-1 downto 0)
+	  );
+end signed_divider;
+
+architecture cheat_divider of signed_divider is
+
+signal signed_long_output: std_logic_vector(SIGNAL_LENGTH-1 downto 0);
+
+constant zeros : std_logic_vector(SIGNAL_LENGTH-1 downto 0) := (others => '0');
+
+begin
+
+process(input_A, input_B)
+begin
+    case input_B is
+	     when zeros => signed_long_output <= zeros;
+        when others => signed_long_output <= std_logic_vector(signed(input_A) / signed(input_B));
+    end case;
+end process;
+
+output <= signed_long_output(SIGNAL_LENGTH-1 downto 0);
+
+end cheat_divider;
+
--- vhdl_project/signed_divider/signed_divider_tb.vhd
+++ vhdl_project/signed_divider/signed_divider_tb.vhd
@@ -2,15 +2,15 @@
 -- Company: 
 -- Engineer:
 --
--- Create Date:   16:05:26 03/23/2018
+-- Create Date:   16:41:43 03/23/2018
 -- Design Name:   
--- Module Name:   U:/git/biquad_filter/vhdl_project/signed_multiplier/signed_multiplier_tb.vhd
+-- Module Name:   U:/git/biquad_filter/vhdl_project/signed_divider/signed_divider_tb.vhd
 -- Project Name:  biquad_filter
 -- Target Device:  
 -- Tool versions:  
 -- Description:   
 -- 
--- VHDL Test Bench Created by ISE for module: signed_multiplier
+-- VHDL Test Bench Created by ISE for module: signed_divider
 -- 
 -- Dependencies:
 -- 
@@ -32,16 +32,15 @@ USE ieee.std_logic_1164.ALL;
 -- arithmetic functions with Signed or Unsigned values
 USE ieee.numeric_std.ALL;
  
-ENTITY signed_multiplier_tb IS
-END signed_multiplier_tb;
+ENTITY signed_divider_tb IS
+END signed_divider_tb;
  
-ARCHITECTURE behavior OF signed_multiplier_tb IS 
+ARCHITECTURE behavior OF signed_divider_tb IS 
  
     -- Component Declaration for the Unit Under Test (UUT)
- 
     constant SIGNAL_LENGTH_test : integer := 16;
 
-    COMPONENT signed_multiplier
+    COMPONENT signed_divider
     generic ( SIGNAL_LENGTH: positive);
     PORT(
          input_A : IN  std_logic_vector(SIGNAL_LENGTH-1 downto 0);
@@ -62,18 +61,17 @@ ARCHITECTURE behavior OF signed_multiplier_tb IS
    signal en : std_logic := '0';
 
  	--Outputs
-   signal output_test1 : std_logic_vector(SIGNAL_LENGTH_test-1 downto 0);
+   signal output1_test : std_logic_vector(SIGNAL_LENGTH_test-1 downto 0);
 
    -- Clock period definitions
    constant clk_period : time := 10 ns;
-	
-	for uut1 : signed_multiplier use entity
-            work.signed_multiplier(cheat_multiplier);
- 
+   
+	for uut1 : signed_divider use entity
+            work.signed_divider(cheat_divider);
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
-   uut1: signed_multiplier
+   uut1: signed_divider
 	generic map ( SIGNAL_LENGTH => SIGNAL_LENGTH_test)
 	PORT MAP (
           input_A => input_A_test,
@@ -81,22 +79,12 @@ BEGIN
           clk => clk,
           reset => reset,
           en => en,
-          output => output_test1
+          output => output1_test
         );
 
    -- Clock process definitions
    clk_process :process
    begin
-		clk <= '0';
-		wait for clk_period/2;
-		clk <= '1';
-		wait for clk_period/2;
-   end process;
- 
-
-   -- Stimulus process
-   stim_proc: process
-   begin		
       -- hold reset state for 100 ns.
       wait for 100 ns;	
 		reset <= '1';
@@ -106,20 +94,20 @@ BEGIN
 
       -- insert stimulus here 
 		en <= '1';
-		input_A_test <= std_logic_vector(to_signed(63, input_A_test'length));
+		input_A_test <= std_logic_vector(to_signed(64, input_A_test'length));
 		input_B_test <= std_logic_vector(to_signed(64, input_B_test'length));
 		wait for 100 ns;
 		
-		input_A_test <= std_logic_vector(to_signed(-3, input_A_test'length));
-		input_B_test <= std_logic_vector(to_signed(4, input_B_test'length));
+		input_A_test <= std_logic_vector(to_signed(-64, input_A_test'length));
+		input_B_test <= std_logic_vector(to_signed(64, input_B_test'length));
 		wait for 100 ns;
 		
-		input_A_test <= std_logic_vector(to_signed(3, input_A_test'length));
-		input_B_test <= std_logic_vector(to_signed(4, input_B_test'length));
+		input_A_test <= std_logic_vector(to_signed(128, input_A_test'length));
+		input_B_test <= std_logic_vector(to_signed(64, input_B_test'length));
 		wait for 100 ns;
 		
-		input_A_test <= std_logic_vector(to_signed(-4, input_A_test'length));
-		input_B_test <= std_logic_vector(to_signed(3, input_B_test'length));
+		input_A_test <= std_logic_vector(to_signed(128, input_A_test'length));
+		input_B_test <= std_logic_vector(to_signed(-64, input_B_test'length));
 		wait for 100 ns;
 		
 		input_A_test <= std_logic_vector(to_signed(-64, input_A_test'length));
@@ -128,6 +116,10 @@ BEGIN
 		
 		input_A_test <= std_logic_vector(to_signed(0, input_A_test'length));
 		input_B_test <= std_logic_vector(to_signed(0, input_B_test'length));
+		wait for 100 ns;
+		
+		input_A_test <= std_logic_vector(to_signed(6, input_A_test'length));
+		input_B_test <= std_logic_vector(to_signed(13, input_B_test'length));
 		wait for 100 ns;
 		wait;
    end process;
