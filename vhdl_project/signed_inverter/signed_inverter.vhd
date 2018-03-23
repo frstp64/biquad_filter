d615c33290e6616bfd8f4d2e199c12af3b87b9eb
Frederic St-Pierre <frederic.st-pierre.7@ulaval.ca>
2018-03-22 21:51:01 -0400

    organized files a little bit, wrote the signed_inverted, not tested yet

---
+++ vhdl_project/signed_inverter/signed_inverter.vhd
@@ -0,0 +1,50 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    21:25:08 03/22/2018 
+-- Design Name: 
+-- Module Name:    signed_inverter - combinatorial 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity signed_inverter is
+    generic ( SIGNAL_LENGTH: positive);
+    Port ( input_value : in  STD_LOGIC_VECTOR (SIGNAL_LENGTH-1 downto 0);
+           output_value : out  STD_LOGIC_VECTOR (SIGNAL_LENGTH-1 downto 0));
+end signed_inverter;
+
+architecture combinatorial of signed_inverter is
+
+signal inversion: std_logic_vector (SIGNAL_LENGTH-1 downto 0);
+signal carry: std_logic_vector(SIGNAL_LENGTH-1 downto 0);
+begin
+
+-- implicit half adders to do a simple increment
+carry(0) <= '1';
+output <= inversion xor carry;
+carry(SIGNAL_LENGTH-1 downto 1) <= inversion and carry(SIGNAL_LENGTH-2 downto 0);
+
+end combinatorial;
+
